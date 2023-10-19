de6a8ec1e3883ec9ac0ef0f22ffdc7186d80fba8
Code Sandbox <hello@codesandbox.io>
2023-10-19 15:38:49 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.f287dceff86e31be0ac8a4837b4700c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9a8a6ec41dce64e31e1fa24ee7e0aa",
+    "url": "https://govindsuman-cmd.github.io/csb-4r8xht/index.html"
+  },
+  {
+    "revision": "f779b91736387d684963",
+    "url": "https://govindsuman-cmd.github.io/csb-4r8xht/static/css/main.96f7de54.chunk.css"
+  },
+  {
+    "revision": "fb5b9828dcf3c8181075",
+    "url": "https://govindsuman-cmd.github.io/csb-4r8xht/static/js/2.5aa49a36.chunk.js"
+  },
+  {
+    "revision": "f779b91736387d684963",
+    "url": "https://govindsuman-cmd.github.io/csb-4r8xht/static/js/main.aa438493.chunk.js"
+  },
+  {
+    "revision": "276b975129a9b3ded68a",
+    "url": "https://govindsuman-cmd.github.io/csb-4r8xht/static/js/runtime~main.9b3ea6bd.js"
+  }
+]);
